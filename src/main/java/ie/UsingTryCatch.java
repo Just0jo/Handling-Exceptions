09c421c49cc007394e84ca15b56027ebe5bd5c9b
Just0jo <G00407342@atu.ie>
2022-11-07 10:28:45 +0000

    looping and only allowing positive figures

--- src/main/java/ie/UsingTryCatch.java
+++ src/main/java/ie/UsingTryCatch.java
@@ -5,31 +5,36 @@ import java.util.Scanner;
 
 
 
-    public class UsingTryCatch {
-        public static void main(String[] args) {
+    public class UsingTryCatch
+    {
+        public static void main(String[] args)
+        {
             int num = 1;
             while(num > 0)
-            try {
-                System.out.println("Please enter a number between 0 - 9 :");
-                Scanner myscan = new Scanner(System.in);
-                int myNum = myscan.nextInt();
-
-                if (myNum <= 9 && myNum > 0) {
-                    System.out.println("You entered " + myNum);
-                    num--;
-
-                } else {
-                    System.out.println("Not a valid number");
-                }
-            }
-
-
-
-            catch(InputMismatchException x)
             {
-                System.out.println("Error Received");
+                try
+                {
+                    System.out.println("Please enter a number between 0 - 9 :");
+                    Scanner myscan = new Scanner(System.in);
+                    int myNum = myscan.nextInt();
+
+                    if (myNum <= 9 && myNum > 0)
+                    {
+                        System.out.println("You entered " + myNum);
+                        num--;
+
+                    } else
+                    {
+                        System.out.println("Not a valid number");
+                    }
+
+                }
+                catch (InputMismatchException x)
+                {
+                    System.out.println("Error Received");
 
 
+                }
             }
 
 
