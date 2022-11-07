527c88753cde335a54711c1d093e87e4abf75e45
Just0jo <G00407342@atu.ie>
2022-11-07 10:15:25 +0000

    handling exceptions looping

--- src/main/java/ie/UsingTryCatch.java
+++ src/main/java/ie/UsingTryCatch.java
@@ -7,6 +7,8 @@ import java.util.Scanner;
 
     public class UsingTryCatch {
         public static void main(String[] args) {
+            int num = 1;
+            while(num > 0)
             try {
                 System.out.println("Please enter a number between 0 - 9 :");
                 Scanner myscan = new Scanner(System.in);
@@ -14,6 +16,7 @@ import java.util.Scanner;
 
                 if (myNum <= 9 || myNum > 0) {
                     System.out.println("You entered " + myNum);
+                    num--;
 
                 } else {
                     System.out.println("Not a valid number");
