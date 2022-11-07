732b5612e64802cf03e9a4b9d6c72b4f816e14cb
Just0jo <G00407342@atu.ie>
2022-11-07 10:00:47 +0000

    handling exceptions

--- src/main/java/UsingTryCatch.java
+++ src/main/java/UsingTryCatch.java
@@ -3,20 +3,27 @@ import java.util.Scanner;
 
 public class UsingTryCatch {
     public static void main(String[] args) {
+       try {
+           System.out.println("Please enter a number between 0 - 9 :");
+           Scanner myscan = new Scanner(System.in);
+           int myNum = myscan.nextInt();
 
-            System.out.println("Please enter a number between 0 - 9 :");
-            Scanner myscan = new Scanner(System.in);
-            int myNum = myscan.nextInt();
+           if (myNum <= 9 || myNum > 0) {
+               System.out.println("You entered " + myNum);
 
-            if (myNum <= 9 || myNum > 0) {
-                System.out.println("You entered " + myNum);
-
-            } else {
-                System.out.println("Not a valid number");
-            }
+           } else {
+               System.out.println("Not a valid number");
+           }
+       }
 
 
 
+        catch(InputMismatchException x)
+        {
+            System.out.println("Error Received");
+
+
+        }
 
         }
     }
